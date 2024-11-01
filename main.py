6b88de99158c77af60c63f854d8d3e52355d92fc
sb74 <snbr74@gmail.com>
2024-11-01 23:01:29 +0000

    working game loop

--- main.py
+++ main.py
@@ -5,8 +5,16 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 def main():
     pygame.init()
     print("Starting asteroids!")
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    # game loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
 
 
 if __name__ == "__main__":
