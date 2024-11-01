0f3cadae22e3a2f00923a9fcedc66eb24d15f5cf
sb74 <snbr74@gmail.com>
2024-11-01 23:20:01 +0000

    added clock

--- main.py
+++ main.py
@@ -10,11 +10,16 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     # game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        clock.tick(60)
+        dt = clock.tick() / 1000
 
 
 if __name__ == "__main__":
